e1618d2819dd7a2119b28012f6aa95cd4b2851e1
sshazly <sherif.a.shazly@nasa.gov>
2021-08-04 13:18:23 -0400

    successfully built version, still needs C++ code to ssh into computer to run ros2 launch command

--- src/launch.cpp
+++ src/launch.cpp
@@ -1,10 +1,47 @@
 #include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/time.hpp"
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
+    srand(time(NULL));
+    std::string nodeName = "node_launcher" + std::to_string(rand()%1000000000000);
+    rclcpp::Node nodeHandle(nodeName);
 
-  printf("hello world launch_node package\n");
-  return 0;
+    nodeHandle.declare_parameter("package");
+    nodeHandle.declare_parameter("launch_file");
+    nodeHandle.declare_parameter("launch_args");
+    nodeHandle.declare_parameter("ip");
+    nodeHandle.declare_parameter("user");
+    nodeHandle.declare_parameter("password");
+
+    std::string package_name;
+    std::string launch_file_name;
+    std::string launch_args_name;
+    std::string ip_name;
+    std::string user_name;
+    std::string password_name;
+
+
+    nodeHandle.get_parameter("package", package_name);              
+    nodeHandle.get_parameter("launch_file", launch_file_name);     
+    nodeHandle.get_parameter("launch_args", launch_args_name);     
+    nodeHandle.get_parameter("ip", ip_name);              
+    nodeHandle.get_parameter("user", user_name);              
+    nodeHandle.get_parameter("password", password_name);     
+
+
+    // ssh into a computer here if the other parameters are provided
+    std::string launch_cmd = "ros2 launch " + package_name + " " + launch_file_name + " " + launch_args_name;
+    // Get User Data
+    if(!ip_name.empty() && !user_name.empty() && !password_name.empty())
+    {
+      // ssh into computer and run command
+    }
+    else
+    {
+      std::printf("running: %s", launch_cmd.c_str());
+      system(launch_cmd.c_str());
+    }
+    return 0;
 }
